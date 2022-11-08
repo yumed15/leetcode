d7eb155bf0cd58291363d7889235e9caef174b98
Diana Darie <dianadarie96@gmail.com>
2022-11-08 19:49:32 +0000

    Add problem 968

--- src/trees/BinaryTreeCameras.java
+++ src/trees/BinaryTreeCameras.java
@@ -1,9 +1,24 @@
 package trees;
 
-import java.util.HashMap;
-
 // 968. Binary Tree Cameras
-public class BinaryTreeCameras {
-
-
+class BinaryTreeCameras {
+    private static final int NOT_COVERED = 0;
+    private static final int IS_COVERED = 1;
+    private static final int NEEDS_CAMERA = 2;
+    private int res;
+    public int minCameraCover(TreeNode root) {
+        if(dfs(root) == NOT_COVERED) res++;
+        return res;
+    }
+    private int dfs(TreeNode node){
+        if(node == null) return IS_COVERED;
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        if(left == NOT_COVERED || right == NOT_COVERED){
+            res++;
+            return NEEDS_CAMERA;
+        }
+        if(left == NEEDS_CAMERA || right == NEEDS_CAMERA) return IS_COVERED;
+        return NOT_COVERED;
+    }
 }
